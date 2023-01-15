ede1234cf8a05e22d8dea037b4323c2ff07d5dbe
DereainP <93568786+DereainP@users.noreply.github.com>
2023-01-15 13:41:32 -0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,60 @@
+question_data = [{"category": "History", "type": "boolean", "difficulty": "hard",
+                  "question": "Japan was part of the Allied Powers during World War I.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "History", "type": "boolean", "difficulty": "easy",
+                  "question": "The Cold War ended with Joseph Stalin&#039;s death.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "History", "type": "boolean", "difficulty": "easy",
+                  "question": "The Spitfire originated from a racing plane.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "History", "type": "boolean", "difficulty": "hard",
+                  "question": "Joseph Stalin&#039;s real name was Ioseb"
+                              " Bessarionis dze "
+                              "Dzugashvili.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "History", "type": "boolean", "difficulty": "medium",
+                  "question": "&quot;I disapprove of what you say, but I"
+                              " will defend to "
+                              "the death your right to say it&quot; is a quote from "
+                              "French philosopher Voltaire.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "History", "type": "boolean", "difficulty": "medium",
+                  "question": "Brezhnev was the 5th leader of the USSR.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "History", "type": "boolean", "difficulty": "easy",
+                  "question": "United States President John F. Kennedy was "
+                              "assassinated "
+                              "during his presidential motorcade in Atlanta, "
+                              "Georgia on "
+                              "November 22nd, 1963.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "History", "type": "boolean", "difficulty": "hard",
+                  "question": "The fourth funnel of the RMS Titanic was fake designed "
+                              "to "
+                              "make the ship look more powerful and symmetrical.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "History", "type": "boolean", "difficulty": "hard",
+                  "question": "The Battle of Trafalgar took place on October 23rd, 1805",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "History", "type": "boolean", "difficulty": "medium",
+                  "question": "The Hundred Years&#039; War was fought for more than"
+                              " a hundred years.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "History", "type": "boolean", "difficulty": "easy",
+                  "question": "Adolf Hitler was tried at the Nuremberg trials.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "History", "type": "boolean", "difficulty": "hard",
+                  "question": "The USS Missouri (BB-63) last served in the Korean War.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "History", "type": "boolean", "difficulty": "easy",
+                  "question": "The French Kingdom helped the United States gain their"
+                              " independence over Great Britain during "
+                              "the Revolutionary War.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "History", "type": "boolean", "difficulty": "easy",
+                  "question": "The United States of America was the first country "
+                              "to launch a man into space.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "History", "type": "boolean", "difficulty": "medium",
+                  "question": "Franz Joseph I was the last emperor of Austria.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]}]
